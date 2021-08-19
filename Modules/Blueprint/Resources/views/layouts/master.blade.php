da5e27ee89fc0e5e63ce7a96249bfc422204fc3a
mylesmalley <myles@mylesmalley.ca>
2021-08-19 13:28:00 -0300

    updated blueprint to use new stylesheet and malley menu

--- Modules/Blueprint/Resources/views/layouts/master.blade.php
+++ Modules/Blueprint/Resources/views/layouts/master.blade.php
@@ -1,19 +1,53 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
     <head>
+        <title>Malley Blueprint</title>
+        <!-- Required meta tags -->
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Blueprint</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-       {{-- Laravel Mix - CSS File --}}
-       {{-- <link rel="stylesheet" href="{{ mix('css/blueprint.css') }}"> --}}
+        <link rel="stylesheet" href="{{ mix('css/homepage.css') }}" >
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        @includeIf('homepage::googleAnalytics')
+        @livewireStyles
+
+        @yield('stylesheet')
 
     </head>
     <body>
-        @yield('content')
 
-        {{-- Laravel Mix - JS File --}}
-        {{-- <script src="{{ mix('js/blueprint.js') }}"></script> --}}
+    @includeIf('homepage::malleyMenu')
+
+    <br />
+
+
+    <h4 class="text-secondary text-center">Malley Blueprint</h4>
+
+        @if( session('success') )
+            <div class="row">
+                <div class="col-6 offset-3">
+                    <div class="card border-success text-white bg-success">
+                        <div class="card-header">
+                            <strong>Success</strong> :  {{ session('success') }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+
+        <div class='container'>
+
+            @yield('content')
+
+        </div>
+
+        <script src="{{ mix('js/homepage.js') }}"></script>
+
+        @livewireScripts
+
+        @stack('scripts')
+
     </body>
 </html>
