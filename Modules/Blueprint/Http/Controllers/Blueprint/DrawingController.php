b299629824fe6e9efeeb061b0da198e43e34046d
mylesmalley <myles@mylesmalley.ca>
2021-09-20 15:59:35 -0300

    job dispatching creating image renders as expected

--- Modules/Blueprint/Http/Controllers/Blueprint/DrawingController.php
+++ Modules/Blueprint/Http/Controllers/Blueprint/DrawingController.php
@@ -7,11 +7,15 @@ use App\Models\Blueprint;
 use App\Models\FormElementItem;
 use App\Models\Media;
 use App\Http\Controllers\Controller;
+use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
+use Illuminate\Bus\Batch;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Bus;
 use Imagick;
 use ImagickException;
 use Modules\Blueprint\Jobs\ProcessDrawing;
 use Spatie\MediaLibrary\MediaCollections\Exceptions\FileCannotBeAdded;
+use Throwable;
 
 
 class DrawingController extends Controller
@@ -26,44 +30,44 @@ class DrawingController extends Controller
     public function assemble( Blueprint $blueprint, FormElement $formElement )
     {
 
-        $allMedia = FormElementItem::where('form_element_id', $formElement->id)
-            ->pluck('media_id');
-
-        $activeMedia = $blueprint->activeDrawingIDs();
-
-        $usedMedia = Media::whereIn('id', $allMedia->intersect( $activeMedia ) )
-            ->get();
-
-
-        $images = [];
-
-        foreach( $usedMedia as $item )
-        {
-            $images[] = $item->cdnUrl();
-        }
-
-
-        $base = new Imagick( $images[0] );
-        $base->setImageFormat('png');
-//        $base->setImageAlpha();
-
-        for ( $i = 1; $i < count( $images ); $i++ )
-        {
-            $layer = new Imagick( $images[$i] );
-            $base->addImage( $layer );
-        }
-
-
-        $result = $base->mergeImageLayers(imagick::LAYERMETHOD_UNDEFINED);
-
-        try {
-            $blueprint->addMediaFromStream($result->getImageBlob())
-                ->usingName('merged_images')
-                ->usingFileName("merged_images" . '.png')
-                ->toMediaCollection('tests', 's3');
-        } catch (ImagickException | FileCannotBeAdded $e) {
-            dd($e);
-        }
+//        $allMedia = FormElementItem::where('form_element_id', $formElement->id)
+//            ->pluck('media_id');
+//
+//        $activeMedia = $blueprint->activeDrawingIDs();
+//
+//        $usedMedia = Media::whereIn('id', $allMedia->intersect( $activeMedia ) )
+//            ->get();
+//
+//
+//        $images = [];
+//
+//        foreach( $usedMedia as $item )
+//        {
+//            $images[] = $item->cdnUrl();
+//        }
+//
+//
+//        $base = new Imagick( $images[0] );
+//        $base->setImageFormat('png');
+////        $base->setImageAlpha();
+//
+//        for ( $i = 1; $i < count( $images ); $i++ )
+//        {
+//            $layer = new Imagick( $images[$i] );
+//            $base->addImage( $layer );
+//        }
+//
+//
+//        $result = $base->mergeImageLayers(imagick::LAYERMETHOD_UNDEFINED);
+//
+//        try {
+//            $blueprint->addMediaFromStream($result->getImageBlob())
+//                ->usingName('merged_images')
+//                ->usingFileName("merged_images" . '.png')
+//                ->toMediaCollection('tests', 's3');
+//        } catch (ImagickException | FileCannotBeAdded $e) {
+//            dd($e);
+//        }
 
     }
 
@@ -79,16 +83,33 @@ class DrawingController extends Controller
     }
 
 
-
-
-    public function test( Blueprint $blueprint )
+    /**
+     * @throws Throwable
+     */
+    public function test(Blueprint $blueprint )
     {
         $image_blocks = $blueprint->platform->drawingElements()->get();
 
-//        dd( $image_blocks );
+        $events = [];
+
+        foreach( $image_blocks as $block )
+        {
+            $events = ProcessDrawing::dispatch( $blueprint, $block );
+        }
 
 
-        ProcessDrawing::dispatch();
+        Bus::batch($events)
+            ->then(function (Batch $batch) {
+
+
+        })->catch(function (Batch $batch, Throwable $e) {
+                Bugsnag::notifyException($e);
+
+            })->finally(function (Batch $batch) {
+
+            })->dispatch();
+
+
 
 
     }
--- Modules/Blueprint/Jobs/ProcessDrawing.php
+++ Modules/Blueprint/Jobs/ProcessDrawing.php
@@ -2,31 +2,37 @@
 
 namespace Modules\Blueprint\Jobs;
 
+use App\Models\FormElement;
 use App\Models\FormElementItem;
+use App\Models\Media;
+use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use App\Models\Blueprint;
-
+use ImagickException;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileCannotBeAdded;
+use Imagick;
+use Illuminate\Bus\Batchable;
 
 class ProcessDrawing implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, Batchable;
 
     protected Blueprint $blueprint;
-    protected FormElementItem $formElementItem;
+    protected FormElement $formElement;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct( Blueprint $blueprint, FormElementItem $formElementItem )
+    public function __construct( Blueprint $blueprint, FormElement $formElement )
     {
         $this->blueprint = $blueprint;
-        $this->formElementItem = $formElementItem;
+        $this->formElement = $formElement;
     }
 
 
@@ -34,11 +40,49 @@ class ProcessDrawing implements ShouldQueue
      * Execute the job.
      *
      * @return void
+     * @throws ImagickException
      */
     public function handle()
     {
 
+        $allMedia = FormElementItem::where('form_element_id', $this->formElement->id)
+            ->pluck('media_id');
 
+        $activeMedia = $this->blueprint->activeDrawingIDs();
+
+        $usedMedia = Media::whereIn('id', $allMedia->intersect( $activeMedia ) )
+            ->get();
+
+
+        $images = [];
+
+        foreach( $usedMedia as $item )
+        {
+            $images[] = $item->cdnUrl();
+        }
+
+
+        $base = new Imagick( $images[0] );
+        $base->setImageFormat('png');
+
+
+        for ( $i = 1; $i < count( $images ); $i++ )
+        {
+            $layer = new Imagick( $images[$i] );
+            $base->addImage( $layer );
+        }
+
+
+        $result = $base->mergeImageLayers(imagick::LAYERMETHOD_UNDEFINED);
+
+        try {
+            $this->blueprint->addMediaFromStream($result->getImageBlob())
+                ->usingName(str_replace( [' ','.','(',')',':',','], ['_'], $this->formElement->label ))
+                ->usingFileName(str_replace( [' ','.','(',')',':',','], ['_'], $this->formElement->label ) . '.png')
+                ->toMediaCollection('test2', 's3');
+        } catch (ImagickException | FileCannotBeAdded $e) {
+            Bugsnag::notifyException($e);
+        }
 
 
 
