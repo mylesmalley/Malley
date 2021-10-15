a75fcde2dd73aed967808b5b130017c249d5433e
mylesmalley <myles@mylesmalley.ca>
2021-10-15 12:12:25 -0300

    added in support for QC labels

--- Modules/Vehicles/Http/Controllers/Documents/StickerController.php
+++ Modules/Vehicles/Http/Controllers/Documents/StickerController.php
@@ -4,7 +4,7 @@ namespace Modules\Vehicles\Http\Controllers\Documents;
 
 use App\Http\Controllers\Controller;
 use App\Models\Vehicle;
-use \Illuminate\View\View;
+use Illuminate\View\View;
 
 /**
  * Class VehiclesSerialsController
@@ -14,13 +14,14 @@ class StickerController extends Controller
 {
     /**
      * @param Vehicle $vehicle
+     * @param string|null $location
      * @return View
      */
-    public function show(Vehicle $vehicle, bool $US = false): View
+    public function show(Vehicle $vehicle, string $location = null): View
     {
         return view('vehicles::stickers.show', [
                 'vehicle' => $vehicle,
-                'us' => $US,
+                'location' => $location,
             ]);
     }
 
