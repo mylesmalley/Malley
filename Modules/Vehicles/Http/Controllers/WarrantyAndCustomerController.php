19e9e3b209699932b7b1fbb444277678a3223ad6
mylesmalley <myles@mylesmalley.ca>
2021-10-30 17:26:19 -0300

    handing updating warranty registration dates

--- Modules/Vehicles/Http/Controllers/WarrantyAndCustomerController.php
+++ Modules/Vehicles/Http/Controllers/WarrantyAndCustomerController.php
@@ -4,8 +4,11 @@ namespace Modules\Vehicles\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Vehicle;
+use App\Models\VehicleDate;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 use View;
 use Mpdf\Mpdf;
 
@@ -35,8 +38,79 @@ class WarrantyAndCustomerController extends Controller
     public function update( Request $request,  Vehicle $vehicle ): RedirectResponse
     {
 
-        $vehicle->update( $request->all() );
+        $vehicle->update( $request->except([
+            'date_of_purchase',
+            'date_warranty_registered',
+            'date_warranty_expiry'
+        ]) );
         $vehicle->save();
+
+
+        if ( $request->input('date_of_purchase') )
+        {
+            VehicleDate::where('vehicle_id', '=', $vehicle->id)
+                ->where('name', '=', 'of_purchase')
+                ->update([
+                    'current' => 0
+                ]);
+
+            VehicleDate::create([
+                'vehicle_id' => $vehicle->id,
+                'user_id' => Auth::user()->id,
+                'timestamp' => Carbon::createFromDate( $request->input('date_of_purchase') )->toIso8601String(),
+                'name' => "of_purchase", // name of date field
+                'notes' => "", // use preset notes if provided
+                'update_ford' => 0,
+                'submitted_to_ford' => 0,
+                'current' => 1,
+            ])->save();
+        }
+
+
+        if ( $request->input('date_warranty_registered') )
+        {
+            VehicleDate::where('vehicle_id', '=', $vehicle->id)
+                ->where('name', '=', 'warranty_registered')
+                ->update([
+                    'current' => 0
+                ]);
+
+            VehicleDate::create([
+                'vehicle_id' => $vehicle->id,
+                'user_id' => Auth::user()->id,
+                'timestamp' => Carbon::createFromDate( $request->input('date_warranty_registered') )->toIso8601String(),
+                'name' => "warranty_registered", // name of date field
+                'notes' => "", // use preset notes if provided
+                'update_ford' => 0,
+                'submitted_to_ford' => 0,
+                'current' => 1,
+            ])->save();
+        }
+
+
+
+        if ( $request->input('date_warranty_expiry') )
+        {
+            VehicleDate::where('vehicle_id', '=', $vehicle->id)
+                ->where('name', '=', 'warranty_expiry')
+                ->update([
+                    'current' => 0
+                ]);
+
+            VehicleDate::create([
+                'vehicle_id' => $vehicle->id,
+                'user_id' => Auth::user()->id,
+                'timestamp' => Carbon::createFromDate( $request->input('date_warranty_expiry') )->toIso8601String(),
+                'name' => "warranty_expiry", // name of date field
+                'notes' => "", // use preset notes if provided
+                'update_ford' => 0,
+                'submitted_to_ford' => 0,
+                'current' => 1,
+            ])->save();
+        }
+
+
+
 
         return redirect('/vehicles/'.$vehicle->id );
     }
