48993678e139655718e4d527ee3f100971884c97
mylesmalley <myles@mylesmalley.ca>
2021-11-20 11:25:58 -0400

    basic display of price list

--- Modules/Index/Http/Controllers/Index/PricingController.php
+++ Modules/Index/Http/Controllers/Index/PricingController.php
@@ -13,9 +13,14 @@ class PricingController extends Controller
     public function index(BaseVan $baseVan)
     {
 
-        $options = Option::where('base_van_id', '=', $baseVan->id )
+        $options = Option::select([
+                'id','option_name','option_description','option_price_tier_2',
+                'option_price_tier_3','option_price_msrp_offset',
+                'option_price_dealer_offset'])
+            ->where('base_van_id', '=', $baseVan->id )
             ->where('obsolete', '=', false)
             ->where('has_pricing', '=', true)
+            ->orderBy('option_name', 'ASC')
             ->get();
 
         return view('index::index.pricing.index', [
--- Modules/Index/Resources/views/index/pricing/option_pricing_component.blade.php
+++ Modules/Index/Resources/views/index/pricing/option_pricing_component.blade.php
@@ -1,3 +1,17 @@
 <div>
-    asdf
+    <div class="row">
+        <div class="col-2">
+            {{ $option->option_name }}
+        </div>
+        <div class="col-5">
+            {{ $option->option_description }}
+        </div>
+        <div class="col-2 text-end">
+            {{ $option->price_tier_2 ?? '' }}
+        </div>
+        <div class="col-2 text-end">
+            {{ $option->price_tier_3 ?? '' }}
+
+        </div>
+    </div>
 </div>
