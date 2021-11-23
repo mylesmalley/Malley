a274bebd472b3c4530094a643e169bc5744b6281
mylesmalley <myles@mylesmalley.ca>
2021-11-23 17:12:43 -0400

    worked ide helper update

--- app/Models/TemplateOption.php
+++ app/Models/TemplateOption.php
@@ -2,26 +2,10 @@
 
 namespace App\Models;
 
-use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
-/**
- * App\Models\TemplateOption
- *
- * @property int $id
- * @property int $template_id
- * @property int $option_id
- * @property-read \App\Models\Option|null $option
- * @property-write mixed $created_at
- * @property-write mixed $updated_at
- * @property-read \App\Models\Template $template
- * @method static \Illuminate\Database\Eloquent\Builder|TemplateOption newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|TemplateOption newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|TemplateOption query()
- * @method static \Illuminate\Database\Eloquent\Builder|TemplateOption whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|TemplateOption whereOptionId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|TemplateOption whereTemplateId($value)
- * @mixin \Eloquent
- */
+
 class TemplateOption extends BaseModel
 {
     protected array $fillable= [
@@ -31,21 +15,21 @@ class TemplateOption extends BaseModel
 	
 	public bool $timestamps= false;
 
-	public $table = "template_options";
+	public string $table = "template_options";
 	
 	
 	/**
-	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 * @return BelongsTo
 	 */
-    public function template()
+    public function template(): BelongsTo
     {
     	return $this->belongsTo('App\Models\Template');
     }
 	
 	/**
-	 * @return \Illuminate\Database\Eloquent\Relations\HasOne
+	 * @return HasOne
 	 */
-    public function option()
+    public function option(): HasOne
     {
     	return $this->hasOne('App\Models\Option');
     }
--- app/Models/Wizard.php
+++ app/Models/Wizard.php
@@ -2,44 +2,18 @@
 
 namespace App\Models;
 
-use Eloquent;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-/**
- * App\Models\Wizard
- *
- * @property int $id
- * @property string $name
- * @property int|null $start
- * @property-read Collection|WizardQuestion[] $questions
- * @property-read int|null $questions_count
- * @method static Builder|Wizard newModelQuery()
- * @method static Builder|Wizard newQuery()
- * @method static Builder|Wizard query()
- * @method static Builder|Wizard whereId($value)
- * @method static Builder|Wizard whereName($value)
- * @method static Builder|Wizard whereStart($value)
- * @mixin Eloquent
- * @property int|null $end
- * @property string|null $start_notes
- * @property string|null $end_notes
- * @property string|null $completed_form_option
- * @method static Builder|Wizard whereCompletedFormOption($value)
- * @method static Builder|Wizard whereEnd($value)
- * @method static Builder|Wizard whereEndNotes($value)
- * @method static Builder|Wizard whereStartNotes($value)
- */
+
 class Wizard extends Model
 {
     use HasFactory;
 
-    protected string  $table = 'wizards';
+    protected string $table = 'wizards';
 
-    protected  $fillable = [
+    protected array $fillable = [
         'id',
         'name',
         'start', // id of first question
