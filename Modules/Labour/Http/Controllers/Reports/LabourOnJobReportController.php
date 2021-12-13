63cbf34fbd6080bc550a9c52152e0c169c93455f
mylesmalley <myles@mylesmalley.ca>
2021-12-13 15:24:21 -0400

    rough outline of dates on job

--- Modules/Labour/Http/Controllers/Reports/LabourOnJobReportController.php
+++ Modules/Labour/Http/Controllers/Reports/LabourOnJobReportController.php
@@ -4,6 +4,7 @@ namespace Modules\Labour\Http\Controllers\Reports;
 
 use App\Models\Labour;
 use App\Http\Controllers\Controller;
+use Carbon\CarbonPeriod;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Response;
 
@@ -23,13 +24,62 @@ class LabourOnJobReportController extends Controller
         if ( $job )
         {
             $labour = Labour::where('job', '=', $job )
-                ->with('user','department')
+             //   ->with('user','department')
+                ->orderBy('start','DESC')
                 ->get();
+
+            $unique_users = $labour
+                ->unique('user_id')
+                ->pluck('user_id')
+                ->toArray();
+
+
+            $unique_departments = $labour
+                ->unique('department_id')
+                ->pluck('department_id')
+                ->toArray();
+
+            $unique_departments = array_fill_keys( $unique_departments, 0 );
+
+            //dd( $unique_departments );
+
+            $first = $labour->first()->start;
+            $last = $labour->last()->start;
+
+            $range_els = CarbonPeriod::create( min( $first, $last), '1 day', max($first, $last ) );
+            $range = [];
+
+
+            foreach( $range_els as $date)
+            {
+                $range[] = $date->format('Y-m-d');
+            }
+
+            $by_date_by_dept = array_fill_keys( $range , $unique_departments);
+
+         //   dd( $x );
+
+
+            foreach( $labour as $l )
+            {
+                $date = $l->start->format('Y-m-d');
+                $by_date_by_dept[$date][$l->department_id] += (int) $l->elapsed->totalSeconds;
+            }
+
+
+
+
+
+           // dd( $x );
+            //dd( $unique_users,$unique_departments , $range );
+
+
         }
 
         return response()
             ->view('labour::reports.labour_on_job_report', [
                 'job' => $job,
+                'by_date_by_dept' => $by_date_by_dept,
                 'labour' => $labour,
             ] );
 
--- Modules/Labour/Resources/views/reports/labour_on_job_report.blade.php
+++ Modules/Labour/Resources/views/reports/labour_on_job_report.blade.php
@@ -9,6 +9,32 @@
                 <h1 class="text-center">{{ $job ?? "Pick a Job" }}</h1>
             </div>
         </div>
+
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card border-primary">
+                    <div class="card-header bg-primary text-center text-white">
+                        By Date, By Department
+                    </div>
+
+                    <table>
+
+                        @foreach( $by_date_by_dept as $date => $departments )
+                            <tr>
+                                <td>{{ $date }}</td>
+                                @foreach( $departments as $dept => $time )
+                                    <td>{{ number_format( $by_date_by_dept[$date][$dept], 2) }}</td>
+                                @endforeach
+                            </tr>
+
+                        @endforeach
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 
 @endsection
