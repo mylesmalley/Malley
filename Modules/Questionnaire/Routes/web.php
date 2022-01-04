e63e0d13b47f4c44e622ff50f2f5c046a881018c
mylesmalley <myles@mylesmalley.ca>
2022-01-04 14:41:28 -0400

    fixed bug with controller not being imported into routes file for questionnaire module

--- Modules/Questionnaire/Routes/web.php
+++ Modules/Questionnaire/Routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 
-
+use Modules\Questionnaire\Http\Controllers\QuestionnaireController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('questionnaire')->group(function() {
@@ -9,6 +9,6 @@ Route::prefix('questionnaire')->group(function() {
 
     Route::get('/{wizard}/{blueprint}', 'QuestionnaireController@show');
 
-    Route::post('addAction', [QuestionnaireControoler::class, 'addAction'])
+    Route::post('addAction', [QuestionnaireController::class, 'addAction'])
         ->name('wizard.add_action');
 });
