92432798e5648167deca915a2ededafc1daa9f98
mylesmalley <myles@mylesmalley.ca>
2022-02-16 13:55:56 -0400

    documented trait

--- Modules/Blueprint/Http/Livewire/Form/Traits/HasOptionRules.php
+++ Modules/Blueprint/Http/Livewire/Form/Traits/HasOptionRules.php
@@ -5,12 +5,23 @@ namespace Modules\Blueprint\Http\Livewire\Form\Traits;
     use App\Models\FormElement;
     use Illuminate\Support\Collection;
 
+    /**
+     *  shared code between types of elements in forms to handle whether to show something or not based on rules
+     */
     trait HasOptionRules {
+
+        // array of options that the form element requires to be shown.
         public array $formElementRules = [];
+
+        // boolean if the element is visible or not.
         public bool $show = true;
+
+        // options on the blueprint that are checked against
         public array $referencedOptions = [];
 
         /**
+         * pulls in the related rule if it exists and parses it into an array.
+         *
          * @param FormElement $element
          */
         public function load_rules( FormElement $element ): void
@@ -22,17 +33,27 @@ namespace Modules\Blueprint\Http\Livewire\Form\Traits;
         }
 
 
+        /**
+         * receives the configuration for the vehicle and pulls out the option names turned on
+         * @param Collection $configuration
+         */
         public function set_referenced_options( Collection $configuration ): void
         {
             $this->referencedOptions = $configuration
-                                          //  ->pluck('option_name')
                                             ->toArray();
         }
 
-        public function check_visibility()
+
+        /**
+         *  determines if a form element should be shown or not
+         */
+        public function check_visibility(): void
         {
+            // if no rules present, show it
             if ( count( $this->formElementRules ) === 0) $this->show = true;
+            // if rules present and one of the options overlaps, show it
             else if ( array_intersect( $this->referencedOptions, $this->formElementRules ) ) $this->show = true;
+            // otherwise, don't show it
             else $this->show = false;
         }
     }
