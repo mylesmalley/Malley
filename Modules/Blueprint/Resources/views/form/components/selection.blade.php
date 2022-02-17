7bb44644620ce2e1c4b9e9ad9f450f756189b4e1
mylesmalley <myles@mylesmalley.ca>
2022-02-17 11:15:09 -0400

    restored styling for questions

--- Modules/Blueprint/Resources/views/form/components/selection.blade.php
+++ Modules/Blueprint/Resources/views/form/components/selection.blade.php
@@ -1,7 +1,26 @@
 <div class="row">
-    selection
-{{--    {{ dd( $element ) }}--}}
-    @foreach( $items as $item )
-        {{ $configuration[ $item->option_id ]['description'] }} <br>
-    @endforeach
+    <div class="col-8 offset-2">
+        <div class="card border-primary">
+            <div class="card-header text-white bg-secondary">
+                <h4 class="">{{ $element->label }} - Pick One</h4>
+{{--                {{ $show ? "show" : "hide" }}--}}
+            </div>
+            <div class="card-body">
+
+                <ul class="list-group list-group-flush">
+                    @foreach( $items as $item )
+                        <li
+                                wire:key="configuration-id-{{ $configuration[ $item->option_id ]['id'] }}"
+                                class="list-group-item"
+                                wire:click="toggle({{ $configuration[ $item->option_id ]['id'] }})"
+                        >
+                            @if ( $configuration[ $item->option_id ]['value'] )
+                                <img src="{{ mix('img/checkmark.png') }}" height="14" width="14" alt="selected">
+                            @endif
+                            {{ $configuration[ $item->option_id ]['description'] }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
