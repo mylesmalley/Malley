516a7558f266f8acdc88689caafc8971664f9ed3
mylesmalley <i9u8y7T^>
2022-03-27 17:20:18 -0300

    basic filtering sort of started

--- Modules/Labour/Http/Controllers/ManageLabour/HomeController.php
+++ Modules/Labour/Http/Controllers/ManageLabour/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace Modules\Labour\Http\Controllers\ManageLabour;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Response;
-
+use Illuminate\Http\Request;
+use Modules\Labour\Models\UserDay;
 
 class HomeController extends Controller
 {
@@ -12,8 +15,23 @@ class HomeController extends Controller
     /**
      * @return Response
      */
-    public function home(): Response
+    public function home( Request $request ): Response
     {
+        $request->validate([
+            'start_date' => 'sometimes|date',
+            'end_date' => 'sometimes|date|required_with:start_date',
+            'user_id' => 'sometimes|integer',
+            'department' => 'sometimes|integer',
+        ]);
+
+        $start_date = $request->input('start_date')
+            ?? Carbon::today()->format('Y-m-d');
+
+      //  188
+
+        //dd( UserDay::get( User::find(188), $start_date ));
+
+
         return response()
             ->view('labour::manage_labour.home' );
     }
--- Modules/Labour/Models/UserDay.php
+++ Modules/Labour/Models/UserDay.php
@@ -7,42 +7,16 @@ use App\Models\Labour;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
-use JetBrains\PhpStorm\ArrayShape;
 
 
 class UserDay
 {
-//    public User $user;
-//    public Collection $labour;
-//    public Carbon $date;
-
-
-//    /**
-//     * @param User $user
-//     * @param Carbon|null $date
-//     */
-//    public function __construct( User $user, Carbon $date = null )
-//    {
-//        $this->user = $user;
-//
-//        $this->date = $date ?? Carbon::today();
-//
-//        $this->labour = Labour::where('user_id', $this->user->id )
-//            ->whereDate('start', $this->date )
-//            ->with('user','user.department')
-//            ->orderBy('start')
-//            ->get();
-//    }
-
-
     /**
      * @param User $user
      * @param string $date
      * @return array
      */
-    #[ArrayShape(['user' => "array", 'labour' => "array", 'date' => "mixed", 'dayName' => "mixed", 'monthDay' => "mixed", 'total_elapsed_labour' => "mixed"])]
-//    public static function get(int $user_id, string $date ): array
-    public static function get(User $user, string $date ): array
+   public static function get(User $user, string $date ): array
     {
        return Cache::remember('_user_day_'.$user->id.'-'.$date ,
             Carbon::now()->addDays(30), function() use ($user, $date) {
@@ -99,52 +73,5 @@ class UserDay
 
 
 
-//
-//    public static function getFlagged( int $user_id, string $date ): array|null
-//    {
-//        $user = User::find( $user_id );
-//        $date = Carbon::create($date, 'America/Moncton' );
-//
-//        $rawLabour = Labour::where('user_id', $user->id )
-//            ->where('flagged', true)
-//            ->whereDate('start', $date )
-//            ->with('department')
-//            ->orderBy('start')
-//            ->get();
-//
-//        $labour = [];
-//
-//        if ( count( $labour) === 0) return null;
-//
-//        foreach( $rawLabour as $lab )
-//        {
-//            $labour[] = [
-//                'id' => $lab->id,
-//                'user_id' => $lab->user_id ?? 1,
-//                'job' => $lab->job,
-//                'start' => $lab->start->format('g:i A'),
-//                'end' => ($lab->end) ?  $lab->end->format('g:i A') : null,
-//                'department' => $lab->department->name ?? 'Not Set',
-//                'flagged' => $lab->flagged,
-//                'posted' => $lab->posted,
-//            ] ;
-//        }
-//
-//        return [
-//            'user' => [
-//                'id' => $user->id,
-//                'first_name' => $user->first_name,
-//                'last_name' => $user->last_name,
-//                'department_id' => $user->department_id,
-//                'department' => $user->department->name ?? 'Not Set',
-//            ],
-//            'labour' => $labour,
-//            'date' => $date->format('Y-m-d'),
-//            'dayName' => $date->format('l'),
-//            'monthDay' => $date->format('M d'),
-//
-//        ];
-//    }
-//
 
 }
