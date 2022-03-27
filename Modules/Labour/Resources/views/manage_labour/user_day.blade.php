d7ec1087c0f7389d98a9c216c927828bf762e6b3
mylesmalley <i9u8y7T^>
2022-03-27 20:42:20 -0300

    formatting and added back labour listing

--- Modules/Labour/Resources/views/manage_labour/user_day.blade.php
+++ Modules/Labour/Resources/views/manage_labour/user_day.blade.php
@@ -1,21 +1,78 @@
-<div class="card">
-    <div class="card-header">
-        {{ $ud['user']['first_name'] }} {{ $ud['user']['last_name'] }}
+@php
+    $a_user_day_is_active = request()->has(['selected_user', 'selected_date']);
+    $this_user_day_is_active =
+        request()->has(['selected_user', 'selected_date'])
+        && request()->input('selected_user') == $ud['user']['id']
+        && request()->input('selected_date') ==   $ud['date'];
+@endphp
 
-        @if ( request()->has(['selected_user', 'selected_date'])
-            && request()->input('selected_user') == $ud['user']['id']
-            && request()->input('selected_date') ==   $ud['date'] )
+<div class="card border-secondary">
+    <div class="card-header bg-secondary text-white">
+        {{ $ud['user']['first_name'] }} {{ $ud['user']['last_name'] }} on {{ $ud['monthDay'] }}
+
+        @if ( $a_user_day_is_active )
+            @if ($this_user_day_is_active)
                 selected!!!!
             @endif
-    </div>
-    <div class="card-body">
+        @else
+            no day selected
+        @endif
         <a href="{{ request()->fullUrlWithQuery([
                 'selected_user'=>$ud['user']['id'],
                 'selected_date'=>$ud['date'],
             ]) }}">select</a>
 
+
     </div>
+
+    <table  class="table table-striped table-hover table-sm">
+        <thead>
+            <tr>
+                <th>Job</th>
+                <th>Department</th>
+                <th>Started At</th>
+                <th>Finished At</th>
+                <th>Elapsed</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse( $ud['labour'] as $lab )
+
+                <tr
+                    class="
+{{--                    {{  $lab['id'] === $selectedRow ? 'table-info' : '' }}--}}
+                    {{  $lab['flagged'] ? 'table-warning' : '' }} ">
+
+                    <td>{{ $lab['job'] }}</td>
+                    <td>{{ $lab['department'] ?? "Department" }}</td>
+                    <td>{{ $lab['start'] }}</td>
+
+                    <td>
+                        @if ( ! $lab['end'] )
+                            Ongoing
+                        @else
+                            {{ $lab['end'] }}
+                        @endif
+                    </td>
+
+                    <td>{{ $lab['elapsed'] }}</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="6" class="text-center"> No labour on this date</td>
+                </tr>
+            @endforelse
+        </tbody>
+            <tfoot>
+            <tr>
+                <td colspan="4"></td>
+                <td >{{ $ud['total_elapsed_labour'] }} Hours</td>
+            </tr>
+        </tfoot>
+    </table>
+
 </div>
+<br>
 
 {{--<div class="--}}
 {{--            card border-info">--}}
