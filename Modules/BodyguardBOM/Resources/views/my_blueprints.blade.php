790c950ae8ceefcd0fe7c9d52a1fd1eb3d81aaca
mylesmalley <myles@mylesmalley.ca>
2022-05-03 13:35:01 -0300

    adding in a module for blueprint BOM

---
+++ Modules/BodyguardBOM/Resources/views/my_blueprints.blade.php
@@ -0,0 +1,9 @@
+@extends('bodyguardbom::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('bodyguardbom.name') !!}
+    </p>
+@endsection
